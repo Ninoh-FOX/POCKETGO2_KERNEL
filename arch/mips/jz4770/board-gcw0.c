7de6f5682e6328f7e90c2979ddd4bcfaca6df32b
Ninoh-FOX <ninohfox@gmail.com>
2020-06-21 15:52:21 +0200

    changed brightness default valor according to 15*

--- arch/mips/jz4770/board-gcw0.c
+++ arch/mips/jz4770/board-gcw0.c
@@ -540,7 +540,7 @@ static struct platform_device gcw0_i2c4_gpio_device = {
 static struct platform_pwm_backlight_data gcw0_backlight_pdata = {
 	.polarity = PWM_POLARITY_NORMAL,
 	.max_brightness = 255,
-	.dft_brightness = 145,
+	.dft_brightness = 150,
 	.pwm_period_ns = 30000, /* 25 kHz: outside human hearing range */
 };
 
